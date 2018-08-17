f23a5a9750cf2e069139519f3ba7af65b7a0e711
UserTBS1 <UserTBS1@TBS-CPU-076>
2018-08-17 16:06:17 -0500

    control

---
+++ JavaApplication55/src/prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author UserTBS1
+ */
+public class prueba {
+    
+    public prueba(){
+        System.out.println("prueba subir proyecto al git");
+    }
+    
+    
+}
